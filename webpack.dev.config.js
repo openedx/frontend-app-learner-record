f1347b9ef50b332e6ba852a9f36ffa46d32c7f91
Jason Wesson <jwesson@2u.com>
2024-02-09 04:48:17 +0000

    refactor: move webpack config logic to node_modules via patch-package
    * add JS/JSX compatability to jest and webpack configs

--- webpack.dev.config.js
+++
@@ -1,9 +0,0 @@
-const { createConfig } = require('@edx/frontend-build');
-/** Uncomment the lines below if you wish to use an env.config.js in development */
-// const envConfig = require('./env.config');
-
-const config = createConfig('webpack-dev');
-
-// config.devServer.port = envConfig.PORT || process.env.PORT || 8080;
-
-module.exports = config;
